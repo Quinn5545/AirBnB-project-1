fd41e8b4870e2a245a65dae38e07139576c8f562
Quinlan <quinnbush5@gmail.com>
2023-10-05 14:28:58 -0700

    Spot endpoints done & minor corrections on valids

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -16,6 +16,7 @@ module.exports = (sequelize, DataTypes) => {
       });
       Spot.belongsTo(models.User, {
         foreignKey: "ownerId",
+        as: "Owner",
       });
       Spot.hasMany(models.SpotImage, {
         foreignKey: "spotId",
@@ -39,38 +40,84 @@ module.exports = (sequelize, DataTypes) => {
       address: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
+        validate: {
+          len: [1, 256],
+        },
       },
       city: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          len: [1, 256],
+          isOnlyLettersAndSpaces(value) {
+            if (!/^[A-Za-z\s]+$/.test(value)) {
+              throw new Error("City should only contain letters and spaces.");
+            }
+          },
+        },
       },
       state: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          len: [1, 256],
+          isOnlyLettersAndSpaces(value) {
+            if (!/^[A-Za-z\s]+$/.test(value)) {
+              throw new Error("City should only contain letters and spaces.");
+            }
+          },
+        },
       },
       country: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          len: [1, 256],
+          isOnlyLettersAndSpaces(value) {
+            if (!/^[A-Za-z\s]+$/.test(value)) {
+              throw new Error("City should only contain letters and spaces.");
+            }
+          },
+        },
       },
       lat: {
         type: DataTypes.DECIMAL,
         allowNull: false,
+        unique: true,
+        validate: {
+          isNumeric: true,
+        },
       },
       lng: {
         type: DataTypes.DECIMAL,
         allowNull: false,
+        unique: true,
+
+        validate: {
+          isNumeric: true,
+        },
       },
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          len: [1, 256],
+        },
       },
       description: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          len: [1, 256],
+        },
       },
       price: {
         type: DataTypes.DECIMAL,
         allowNull: false,
+        validate: {
+          isNumeric: true,
+        },
       },
     },
     {
