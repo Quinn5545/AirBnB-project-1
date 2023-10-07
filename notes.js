b796a8b1d93141bf30adc388fc85cebbe4ba47db
Quinlan <quinnbush5@gmail.com>
2023-10-07 14:19:50 -0700

    notes.js changes to make it prettier

--- notes.js
+++ notes.js
@@ -4,15 +4,25 @@ THINGS THAT NEED TO BE FIXED FRANCO
 
 todo
 Create a Spot - Getting rejected even with valid inputs in the body. Make sure you are checking req.body.
+
 IMPORTANT - Unable to test the rest of the Spots feature because of Create a Spot bug.
+
 Create an Image for a Review - Create an Image for a Review - Should be able to add/create Image for own Review on another user’s Spot.
+
 IMPORTANT - Unable to test the rest of the Review Images functionality because of create bug.
+
 Get Reviews of Current User - Spot object should have previewImage instead of SpotImages. Please see API docs.
+
 Get Reviews by Spot Id - Should return all the reviews for the spot not just 1.
+
 ERROR - Create and Edit a Booking - Dates Within Existing Booking - allows another booking with dates inside an existing booking.
+
 Get All Current User’s Bookings - Spot property’s previewImage should just be the url of 1 spot image.
+
 NOT AUTHED - Delete a Booking - Getting: “message”: “Internal Server Error” instead of Forbidden/Authorization error.
+
 Get All Spots With Params - Assuming optional filters were not implemented. Pagination functionality working.
+
 ERROR - Get All Spots With Params - Invalid Params - should catch for invalid page and size query filters - Getting: “error”: “Internal Server Error”
 
 
