e9e86452d697a09957b851b8a85ab4d5c3fbd05d
Quinlan <quinnbush5@gmail.com>
2023-10-24 10:06:43 -0700

    skeleton for get all spots

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { useDispatch } from "react-redux";
 import { Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+import { Route } from "react-router-dom/cjs/react-router-dom.min";
+import HomePage from "./components/HomePage";
 
 function App() {
   const dispatch = useDispatch();
@@ -15,6 +17,9 @@ function App() {
     <>
       <Navigation isLoaded={isLoaded} />
       {isLoaded && <Switch></Switch>}
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </>
   );
 }
