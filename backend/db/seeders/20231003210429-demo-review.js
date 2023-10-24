7fe695829833718e9e97a7e9c3f9fbe657de3d94
Quinlan <quinnbush5@gmail.com>
2023-10-24 09:29:29 -0700

    seeder info expanded

--- backend/db/seeders/20231003210429-demo-review.js
+++ backend/db/seeders/20231003210429-demo-review.js
@@ -19,42 +19,118 @@ module.exports = {
         {
           spotId: 1,
           userId: 1,
-          review: "it's lit -Travis Scott",
+          review:
+            "I had an amazing time at this luxurious villa. The private pool was a highlight, and the views were breathtaking!",
           stars: 5,
         },
         {
           spotId: 1,
-          userId: 1,
-          review: "it's sorta lit -Travis Scott",
-          stars: 3,
-        },
-        {
-          spotId: 2,
           userId: 2,
-          review: "it's not lit -(not)Travis Scott",
-          stars: 1,
-        },
-        {
-          spotId: 2,
-          userId: 2,
-          review: "it's decently lit -Travis Scott",
-          stars: 3,
-        },
-        {
-          spotId: 1,
-          userId: 2,
-          review: "it's decentlyyyy lit -Travis Scott",
+          review:
+            "The villa was nice, but it was a bit pricey for my taste. Still, a good experience overall.",
           stars: 4,
         },
         {
           spotId: 2,
           userId: 1,
-          review: "it's decentlyyyyy lit -Travis Scott",
+          review:
+            "The golf resort getaway was a dream come true. I loved the stunning mountain views and the golfing experience.",
+          stars: 5,
+        },
+        {
+          spotId: 2,
+          userId: 2,
+          review:
+            "I didn't enjoy my stay at the golf resort. The mountain views were nice, but the service could have been better.",
+          stars: 2,
+        },
+        {
+          spotId: 3,
+          userId: 1,
+          review:
+            "The mid-century modern home in Palm Springs was a blast from the past. The architecture and interior were incredible!",
+          stars: 5,
+        },
+        {
+          spotId: 3,
+          userId: 2,
+          review:
+            "The mid-century modern home was a unique experience, but it wasn't my style. Still, it was interesting to stay there.",
+          stars: 3,
+        },
+        {
+          spotId: 4,
+          userId: 1,
+          review:
+            "I had a wonderful time at the desert oasis retreat. The serenity of Indian Wells was truly relaxing, and the property was well-maintained.",
+          stars: 5,
+        },
+        {
+          spotId: 4,
+          userId: 2,
+          review:
+            "The desert oasis retreat was decent, but I expected more for the price. The property was nice, but it didn't fully meet my expectations.",
+          stars: 4,
+        },
+        {
+          spotId: 5,
+          userId: 1,
+          review:
+            "I thoroughly enjoyed my stay at the mountain-view villa in Rancho Mirage. The views were stunning, and the villa was comfortable.",
+          stars: 5,
+        },
+        {
+          spotId: 5,
+          userId: 2,
+          review:
+            "The mountain-view villa was a great choice. The location was convenient, and the amenities were fantastic.",
+          stars: 4,
+        },
+        {
+          spotId: 6,
+          userId: 1,
+          review:
+            "The tropical paradise in Cathedral City was a fantastic escape. The lush garden and pool made it a memorable trip.",
+          stars: 5,
+        },
+        {
+          spotId: 6,
+          userId: 2,
+          review:
+            "The tropical paradise was a lovely experience. The garden was a highlight, but it could use some minor improvements.",
+          stars: 4,
+        },
+        {
+          spotId: 7,
+          userId: 1,
+          review:
+            "The sunset retreat in Coachella was breathtaking. I loved watching the stunning sunsets from the property.",
+          stars: 5,
+        },
+        {
+          spotId: 7,
+          userId: 2,
+          review:
+            "The sunset retreat provided a unique experience, and the sunsets were truly amazing. A great place for relaxation.",
+          stars: 5,
+        },
+        {
+          spotId: 8,
+          userId: 1,
+          review:
+            "I had a relaxing time at the hot springs getaway in Desert Hot Springs. The therapeutic pools were fantastic.",
+          stars: 4,
+        },
+        {
+          spotId: 8,
+          userId: 2,
+          review:
+            "The hot springs getaway was a peaceful escape. The pools were relaxing, but some maintenance was needed.",
           stars: 4,
         },
       ],
       {
-        validate: true, //make sure this is included for every bulkCreate
+        validate: true, // Make sure this is included for every bulkCreate
       }
     );
   },
--- backend/db/seeders/20231003212357-demo-review-image.js
+++ backend/db/seeders/20231003212357-demo-review-image.js
@@ -18,23 +18,39 @@ module.exports = {
       [
         {
           reviewId: 1,
-          url: "111https://people.com/thmb/BaUYzCEEhQ0PotfgTgCZHTI4cx0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/schitts-creek-home-tout-1-4456f21447984bd49592da039895dc0b.jpg",
+          url: "https://images.unsplash.com/photo-1613545325278-f24b0cae1224?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bHV4dXJ5JTIwaG91c2V8ZW58MHx8MHx8fDA%3D",
         },
         {
           reviewId: 2,
-          url: "222https://people.com/thmb/BaUYzCEEhQ0PotfgTgCZHTI4cx0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/schitts-creek-home-tout-1-4456f21447984bd49592da039895dc0b.jpg",
+          url: "https://media.istockphoto.com/id/1333804787/photo/modern-white-interior-design-with-fireplace-and-beautiful-backyard-view.jpg?s=612x612&w=0&k=20&c=y-iu57-SElFVq6-nW0ZvQHQcDnzL9Stq4v6m88jT-sQ=",
         },
         {
           reviewId: 3,
-          url: "333https://media.istockphoto.com/id/504709890/photo/house-burned-in-major-fire.jpg?s=612x612&w=0&k=20&c=kd5htEisBb-Uf809p966Ns8DSijiNApT4cpsnTTCSsA=",
+          url: "https://images.pexels.com/photos/1571459/pexels-photo-1571459.jpeg?cs=srgb&dl=pexels-vecislavas-popa-1571459.jpg&fm=jpg",
         },
         {
           reviewId: 4,
-          url: "444https://media.istockphoto.com/id/504709890/photo/house-burned-in-major-fire.jpg?s=612x612&w=0&k=20&c=kd5htEisBb-Uf809p966Ns8DSijiNApT4cpsnTTCSsA=",
+          url: "https://media.istockphoto.com/id/1142747548/photo/modern-house-interior-design.jpg?s=1024x1024&w=is&k=20&c=k6lYPN64-dDF2gZJl2Hvxq12DaT_1b1r-nR_asEUCP0=",
+        },
+        {
+          reviewId: 5,
+          url: "https://images.unsplash.com/photo-1613082294483-fec382d8367e?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bHV4dXJ5JTIwaG9tZXxlbnwwfHwwfHx8MA%3D%3D",
+        },
+        {
+          reviewId: 6,
+          url: "https://media.istockphoto.com/id/1276567523/photo/wonderful-open-floorplan-in-new-luxury-home-with-black-trim-windows.jpg?s=612x612&w=0&k=20&c=d470_B3BnMBD-7IyceOzwVFAWmXcD0DgSbeT5jeJBpU=",
+        },
+        {
+          reviewId: 7,
+          url: "https://media.istockphoto.com/id/1326994520/photo/we-all-deserve-a-fresh-break-from-the-city.webp?b=1&s=170667a&w=0&k=20&c=S7T1RDGbTjnHTbr20d2KOns1mBBRqFAAZAuJijy02VY=",
+        },
+        {
+          reviewId: 8,
+          url: "https://i.pinimg.com/originals/ec/94/a0/ec94a08704d1fbe418c1c1147bbddcac.jpg",
         },
       ],
       {
-        validate: true, //make sure this is included for every bulkCreate
+        validate: true, // Make sure this is included for every bulkCreate
       }
     );
   },
